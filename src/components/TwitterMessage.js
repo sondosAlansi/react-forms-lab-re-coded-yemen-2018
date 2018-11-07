b7e2a5abe81ecdcfb18523eeff9ed576bc44ee11
sondos alansi <sondos.alansi@yahoo.com>
2018-11-07 07:53:37 +0000

    automatically backed up by learn

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -16,7 +16,7 @@ handleChange=event =>{
     return (
       <div>
         <strong>Your message:</strong>
-        
+
         <input type="text" value={this.state.value} onChange={this.handleChange}/>
         {this.props.maxChars-this.state.value.length}
       </div>
